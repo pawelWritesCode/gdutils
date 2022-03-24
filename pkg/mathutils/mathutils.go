88f5b3455784c11cb01782cd0fad1599cde14e66
Михаил Буслаев <buslaevnmh@yandex.ru>
2022-03-24 23:57:39 +0300

    fix float64 randomizer

--- pkg/mathutils/mathutils.go
+++ pkg/mathutils/mathutils.go
@@ -17,3 +17,11 @@ func RandomInt(from, to int) (int, error) {
 	rand.Seed(time.Now().UnixNano())
 	return rand.Intn(to-from+1) + from, nil
 }
+
+func RandomFloat64(from, to float64) (float64, error) {
+	if to < from {
+		return 0, fmt.Errorf("could not generate random float because %d is less than %d", from, to)
+	}
+
+	return from + rand.Float64() * (from - to), nil
+}
