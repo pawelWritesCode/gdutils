2dce3613035623ed6ca8469999a8f8cf4862475a
Paweł Chmielewski <pawel.chmielewski@knowit.eu>
2022-06-10 16:08:33 +0200

    feature: upgrade type checking

--- doc.go
+++ doc.go
@@ -6,7 +6,7 @@
 //	func NewDefaultAPIContext(isDebug bool, jsonSchemaDir string) *APIContext
 //
 // Second, more customisable returns *APIContext with provided services:
-//	func NewAPIContext(cli *http.Client, c cache.Cache, jv SchemaValidators, p PathFinders, f Formatters, d debugger.Debugger) *APIContext
+//	func NewAPIContext(cli *http.Client, c cache.Cache, jv SchemaValidators, p PathFinders, f Formatters, t TypeMappers, d debugger.Debugger) *APIContext
 //
 // No matter which way you choose, you can inject your custom services afterwards with one of available setters:
 //	func (apiCtx *APIContext) SetDebugger(d debugger.Debugger)
@@ -21,18 +21,21 @@
 //	func (apiCtx *APIContext) SetYAMLPathFinder(r pathfinder.PathFinder)
 //	func (apiCtx *APIContext) SetYAMLFormatter(yd formatter.Formatter)
 //	func (apiCtx *APIContext) SetXMLFormatter(xf formatter.Formatter)
+//	func (apiCtx *APIContext) SetJSONTypeMapper(c types.Checker)
+//	func (apiCtx *APIContext) SetYAMLTypeMapper(c types.Checker)
+//	func (apiCtx *APIContext) SetGoTypeMapper(c types.Checker)
 //
-// Those services will be used in utility methods.
-// For example, if you want to use your own debugger, create your own struct, implement debugger.Debugger interface on it,
-// and then inject it with "func (apiCtx *APIContext) SetDebugger(d debugger.Debugger)" method.
+// Those services will be used in utility methods and can be accessed directly if needed (to use in any custom methods).
+// For example, if you want to use your own debugger - because default one is not suitable for you, create your own struct,
+// implement debugger.Debugger interface on it, and then inject it with "func (apiCtx *APIContext) SetDebugger(d debugger.Debugger)" method.
 //
-// Testing HTTP API usually consist the following aspects:
+// Testing HTTP API usually consist of the following aspects:
 //
 // * Data generation:
 //
 //	func (apiCtx *APIContext) GenerateRandomInt(from, to int, cacheKey string) error
 //	func (apiCtx *APIContext) GenerateFloat64(from, to float64, cacheKey string) error
-//  func (apiCtx *APIContext) GeneratorRandomRunes(charset string) func(from, to int, cacheKey string) error
+//	func (apiCtx *APIContext) GeneratorRandomRunes(charset string) func(from, to int, cacheKey string) error
 //	func (apiCtx *APIContext) GeneratorRandomSentence(charset string, wordMinLength, wordMaxLength int) func(from, to int, cacheKey string) error
 //	func (apiCtx *APIContext) GetTimeAndTravel(t time.Time, timeDirection timeutils.TimeDirection, timeDuration time.Duration, cacheKey string) error
 //	func (apiCtx *APIContext) GenerateTimeAndTravel(timeDirection timeutils.TimeDirection, timeDuration time.Duration, cacheKey string) error
@@ -62,17 +65,17 @@
 //	func (apiCtx *APIContext) AssertNodesExist(dataFormat format.DataFormat, expressionsTemplates string) error
 //	func (apiCtx *APIContext) AssertNodeExists(dataFormat format.DataFormat, exprTemplate string) error
 //	func (apiCtx *APIContext) AssertNodeNotExists(dataFormat format.DataFormat, exprTemplate string) error
-//	func (apiCtx *APIContext) AssertNodeIsType(df format.DataFormat, exprTemplate string, goType string) error
-//	func (apiCtx *APIContext) AssertNodeIsNotType(df format.DataFormat, exprTemplate string, goType string) error
+//	func (apiCtx *APIContext) AssertNodeIsType(dataFormat format.DataFormat, exprTemplate string, inType types.DataType) error
+//	func (apiCtx *APIContext) AssertNodeIsNotType(dataFormat format.DataFormat, exprTemplate string, inType types.DataType) error
 //	func (apiCtx *APIContext) AssertNodeMatchesRegExp(dataFormat format.DataFormat, exprTemplate, regExpTemplate string) error
 //	func (apiCtx *APIContext) AssertNodeNotMatchesRegExp(dataFormat format.DataFormat, exprTemplate, regExpTemplate string) error
 //	func (apiCtx *APIContext) AssertNodeSliceLengthIs(dataFormat format.DataFormat, exprTemplate string, length int) error
 //	func (apiCtx *APIContext) AssertNodeSliceLengthIsNot(dataFormat format.DataFormat, exprTemplate string, length int) error
-//	func (apiCtx *APIContext) AssertNodeIsTypeAndValue(dataFormat format.DataFormat, exprTemplate, dataType, dataValue string) error
+//	func (apiCtx *APIContext) AssertNodeIsTypeAndValue(dataFormat format.DataFormat, exprTemplate string, dataType types.DataType, dataValue string) error
 //	func (apiCtx *APIContext) AssertResponseHeaderExists(name string) error
 //	func (apiCtx *APIContext) AssertResponseHeaderNotExists(name string) error
 //	func (apiCtx *APIContext) AssertResponseHeaderValueIs(name, value string) error
-//  func (apiCtx *APIContext) AssertResponseMatchesSchemaByReference(referenceTemplate string) error
+//	func (apiCtx *APIContext) AssertResponseMatchesSchemaByReference(referenceTemplate string) error
 //	func (apiCtx *APIContext) AssertResponseMatchesSchemaByString(schemaTemplate string) error
 //	func (apiCtx *APIContext) AssertNodeMatchesSchemaByString(dataFormat format.DataFormat, exprTemplate, schemaTemplate string) error
 //	func (apiCtx *APIContext) AssertNodeMatchesSchemaByReference(dataFormat format.DataFormat, exprTemplate, referenceTemplate string) error
@@ -82,7 +85,7 @@
 //
 //	func (apiCtx *APIContext) SaveNode(dataFormat format.DataFormat, exprTemplate, cacheKey string) error
 //	func (apiCtx *APIContext) SaveHeader(name, cacheKey string) error
-//  func (apiCtx *APIContext) Save(valueTemplate, cacheKey string) error
+//	func (apiCtx *APIContext) Save(valueTemplate, cacheKey string) error
 //
 // * Flow control:
 //
@@ -93,4 +96,27 @@
 //	func (apiCtx *APIContext) DebugPrintResponseBody() error
 //	func (apiCtx *APIContext) DebugStart() error
 //	func (apiCtx *APIContext) DebugStop() error
+//
+// Here is example lib usage to test endpoint returning list of ducks gifs:
+//	ac := gdutils.NewDefaultAPIContext(false, "")
+//
+//	if err := ac.RequestPrepare("GET", "https://random-d.uk/api/v2/list", "DUCK_GIFS_LIST"); err != nil {
+//		return err
+//	}
+//
+//	if err := ac.RequestSend("DUCK_GIFS_LIST"); err != nil {
+//    		return err
+//	}
+//
+//	if err := ac.AssertStatusCodeIs(200); err != nil {
+//    		return err
+//	}
+//
+//	if err := ac.AssertResponseFormatIs(format.JSON); err != nil {
+//    		return err
+//	}
+//
+//	if err := ac.AssertNodeIsType(format.JSON, "$.gifs", types.Slice); err != nil {
+//    		return err
+//	}
 package gdutils
--- pkg/reflectutils/reflectutils.go
+++ pkg/reflectutils/reflectutils.go
@@ -6,7 +6,7 @@ import "reflect"
 // IsValueNil checks whether provided Value is nil.
 func IsValueNil(v reflect.Value) bool {
 	nodeKind := v.Kind()
-	if nodeKind == reflect.Ptr || nodeKind == reflect.Map || nodeKind == reflect.Array ||
+	if nodeKind == reflect.Ptr || nodeKind == reflect.Map ||
 		nodeKind == reflect.Chan || nodeKind == reflect.Slice {
 		return v.IsNil()
 	}
