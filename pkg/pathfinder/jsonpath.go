26a3ff9de5a1b29824408ee879013e4e26060d6c
Paweł Chmielewski <pawel.chmielewski@knowit.eu>
2022-06-14 13:46:07 +0200

    feature: implemented new json path engine and depreciated qjson

--- pkg/pathfinder/jsonpath.go
+++ pkg/pathfinder/jsonpath.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/oliveagle/jsonpath"
 	"github.com/pawelWritesCode/qjson"
+	"github.com/tidwall/gjson"
 )
 
 // QJSONFinder represents implementation of JSON path from https://github.com/pawelWritesCode/qjson library
@@ -15,11 +16,14 @@ type QJSONFinder struct{}
 // OliveagleJSONFinder represents implementation of JSON path from https://github.com/oliveagle/jsonpath library
 type OliveagleJSONFinder struct{}
 
+// GJSONFinder represents implementation of JSON path from https://github.com/tidwall/gjson library
+type GJSONFinder struct{}
+
 // DynamicJSONPathFinder is entity that has ability to obtain data from JSON from given expression.
 // Entity knows how to determine whether expression matches
-// https://github.com/pawelWritesCode/qjson or https://github.com/oliveagle/jsonpath syntax
+// https://github.com/tidwall/gjson or https://github.com/oliveagle/jsonpath syntax
 type DynamicJSONPathFinder struct {
-	qjson               QJSONFinder
+	gjson               GJSONFinder
 	oliveagleJSONFinder OliveagleJSONFinder
 }
 
@@ -31,16 +35,20 @@ func NewOliveagleJSONFinder() OliveagleJSONFinder {
 	return OliveagleJSONFinder{}
 }
 
-func NewDynamicJSONPathFinder(qjson QJSONFinder, oliveagleJSONFinder OliveagleJSONFinder) *DynamicJSONPathFinder {
-	return &DynamicJSONPathFinder{qjson: qjson, oliveagleJSONFinder: oliveagleJSONFinder}
+func NewGJSONFinder() GJSONFinder {
+	return GJSONFinder{}
 }
 
-// Find obtains data from jsonBytes according to given expr valid with oliveagle/jsonpath library
+func NewDynamicJSONPathFinder(gjson GJSONFinder, oliveagleJSONFinder OliveagleJSONFinder) *DynamicJSONPathFinder {
+	return &DynamicJSONPathFinder{gjson: gjson, oliveagleJSONFinder: oliveagleJSONFinder}
+}
+
+// Find obtains data from jsonBytes according to given expr valid with pawelWritesCode/qjson library
 func (Q QJSONFinder) Find(expr string, jsonBytes []byte) (any, error) {
 	return qjson.Resolve(expr, jsonBytes)
 }
 
-// Find obtains data from jsonBytes according to given expr valid with pawelWritesCode/qjson library
+// Find obtains data from jsonBytes according to given expr valid with oliveagle/jsonpath library
 func (o OliveagleJSONFinder) Find(expr string, jsonBytes []byte) (any, error) {
 	var jsonData any
 	err := json.Unmarshal(jsonBytes, &jsonData)
@@ -48,11 +56,11 @@ func (o OliveagleJSONFinder) Find(expr string, jsonBytes []byte) (any, error) {
 		return nil, err
 	}
 
-	return jsonpath.JsonPathLookup(any(jsonData), expr)
+	return jsonpath.JsonPathLookup(jsonData, expr)
 }
 
 // Find obtains data from jsonBytes according to given expr.
-// It accepts expr in format acceptable by pawelWritesCode/qjson or oliveagle/jsonpath libraries.
+// It accepts expr in format acceptable by tidwall/gjson or oliveagle/jsonpath libraries.
 func (d DynamicJSONPathFinder) Find(expr string, jsonBytes []byte) (any, error) {
 	if len(expr) == 0 {
 		return nil, fmt.Errorf("json path can't be empty string")
@@ -62,5 +70,24 @@ func (d DynamicJSONPathFinder) Find(expr string, jsonBytes []byte) (any, error) 
 		return d.oliveagleJSONFinder.Find(expr, jsonBytes)
 	}
 
-	return d.qjson.Find(expr, jsonBytes)
+	return d.gjson.Find(expr, jsonBytes)
+}
+
+// Find obtains data from jsonBytes according to given expr valid with tidwall/gjson library
+func (G GJSONFinder) Find(expr string, bytes []byte) (any, error) {
+	if len(expr) == 0 {
+		return nil, fmt.Errorf("provided empty expression")
+	}
+
+	if !gjson.ValidBytes(bytes) {
+		return nil, fmt.Errorf("detected invalid JSON")
+	}
+
+	result := gjson.GetBytes(bytes, expr)
+
+	if !result.Exists() {
+		return nil, fmt.Errorf("could not find node, using expression %s", expr)
+	}
+
+	return result.Value(), nil
 }
