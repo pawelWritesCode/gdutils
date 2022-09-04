12902bc2448370f396d0ce5052c3d916fa88088a
Paweł Chmielewski <pawel.chmielewski@knowit.eu>
2022-09-04 16:37:05 +0200

    feature(37): Make changes in DebuggerService from debugger subpackage

--- pkg/debugger/debugger.go
+++ pkg/debugger/debugger.go
@@ -2,8 +2,14 @@
 package debugger
 
 import (
+	"encoding/json"
 	"fmt"
-	"strings"
+	"io"
+	"os"
+
+	"github.com/hokaccha/go-prettyjson"
+
+	"github.com/pawelWritesCode/gdutils/pkg/format"
 )
 
 // Debugger represents debugger.
@@ -26,12 +32,27 @@ type Debugger interface {
 
 // DebuggerService is utility tool for debugging
 type DebuggerService struct {
+
 	// actualState tells whether debugger is on/off, true = on, false = off.
 	actualState bool
+
+	// isColored determines whether output will be colored
+	isColored bool
+
+	// limit is the maximum number of bytes to be printed.
+	limit uint16
+
+	// writer is place where output will be written.
+	writer io.Writer
 }
 
-func New(isOn bool) *DebuggerService {
-	return &DebuggerService{actualState: isOn}
+func New(isOn, isColored bool, bytesLimit uint16, writer io.Writer) *DebuggerService {
+	return &DebuggerService{actualState: isOn, limit: bytesLimit, writer: writer}
+}
+
+// NewDefault returns *DebuggerService writing to stdOut with output limit up to 3072 bytes
+func NewDefault(isOn bool) *DebuggerService {
+	return &DebuggerService{actualState: isOn, limit: 3072, writer: os.Stdout, isColored: true}
 }
 
 // IsOn tells whether debugging mode is activated.
@@ -56,7 +77,30 @@ func (d *DebuggerService) Reset(isOn bool) {
 
 // Print prints provided info.
 func (d *DebuggerService) Print(info string) {
-	fmt.Printf(strings.Repeat("-", 35) + " DEBUG " + strings.Repeat("-", 35) + "\n")
-	fmt.Println(info)
-	fmt.Printf(strings.Repeat("-", 77) + "\n")
+	_, _ = fmt.Fprintln(d.writer, d.prepareMessage(info))
+}
+
+// prepareMessage makes few modifications to the message.
+func (d *DebuggerService) prepareMessage(info string) string {
+	var output = []byte(info)
+
+	if format.IsJSON([]byte(info)) {
+		var rm json.RawMessage
+		_ = json.Unmarshal([]byte(info), &rm)
+
+		if d.isColored {
+			output, _ = prettyjson.Marshal(rm)
+		} else {
+			output, _ = json.MarshalIndent(rm, "", "\t")
+		}
+	}
+
+	var bytesToPrint uint16
+	if len(output) <= int(d.limit) {
+		bytesToPrint = uint16(len(output))
+	} else {
+		bytesToPrint = d.limit
+	}
+
+	return string(output[:bytesToPrint])
 }
