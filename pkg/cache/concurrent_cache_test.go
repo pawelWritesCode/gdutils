bfaac15dd6a067d3c4d36c88718bd42aa0785819
Paweł <p.t.chmielewski@gmail.com>
2023-10-01 16:22:09 +0200

    feature(49): Re-structure pkg/cache package

--- pkg/cache/concurrent_cache_test.go
+++ pkg/cache/concurrent_cache_test.go
@@ -5,54 +5,6 @@ import (
 	"testing"
 )
 
-func TestDefaultCache_SaveAndGetValue(t *testing.T) {
-	c := NewDefaultCache()
-	c.Save("test", 1)
-	val, err := c.GetSaved("test")
-	if err != nil {
-		t.Errorf("could not obtain saved value %v", err)
-	}
-
-	iVal, ok := val.(int)
-	if !ok {
-		t.Errorf("cache changed preserved item type")
-	}
-
-	if iVal != 1 {
-		t.Errorf("cache changed preserved item value")
-	}
-}
-
-func TestDefaultCache_GetAllValues(t *testing.T) {
-	c := NewDefaultCache()
-	c.Save("test1", 1)
-	c.Save("test2", 2)
-
-	expected := map[string]any{"test1": 1, "test2": 2}
-
-	if !reflect.DeepEqual(c.All(), expected) {
-		t.Errorf("all does not returns all cached values")
-	}
-}
-
-func TestDefaultCache_Reset(t *testing.T) {
-	c := NewDefaultCache()
-	c.Save("test1", 1)
-	c.Save("test2", 2)
-
-	expected := map[string]any{"test1": 1, "test2": 2}
-
-	if !reflect.DeepEqual(c.All(), expected) {
-		t.Errorf("all does not returns all cached values")
-	}
-
-	c.Reset()
-
-	if !reflect.DeepEqual(c.All(), map[string]any{}) {
-		t.Errorf("reset does not work")
-	}
-}
-
 func TestConcurrentCache_Reset(t *testing.T) {
 	c := NewConcurrentCache()
 	c.Save("test1", 1)
