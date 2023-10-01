3864330c1ab4be9c12674d6624fa30f820f13024
Paweł <p.t.chmielewski@gmail.com>
2023-10-01 16:22:09 +0200

    feature(49): Re-structure pkg/cache package

--- pkg/cache/cache.go
+++
@@ -1,8 +0,0 @@
-package cache
-
-import (
-	"errors"
-)
-
-// ErrMissingKey occurs when cache doesn't have any value under given key.
-var ErrMissingKey = errors.New("missing key")
--- pkg/cache/concurrent_cache.go
+++ pkg/cache/concurrent_cache.go
@@ -1,9 +1,13 @@
 package cache
 
 import (
+	"errors"
 	"fmt"
 	"sync"
 )
+
+// ErrMissingKey occurs when cache doesn't have any value under given key.
+var ErrMissingKey = errors.New("missing key")
 
 // ConcurrentCache is entity that has ability to store and retrieve arbitrary values.
 // Safe for concurrent use.
@@ -14,23 +18,27 @@ type ConcurrentCache struct {
 // NewConcurrentCache returns pointer to ConcurrentCache safe for concurrent use
 func NewConcurrentCache() *ConcurrentCache { return &ConcurrentCache{buff: sync.Map{}} }
 
+// Save persist value under given key
 func (c *ConcurrentCache) Save(key string, value any) {
 	c.buff.Store(key, value)
 }
 
+// GetSaved returns previously saved value under given key or ErrMissingKey
 func (c *ConcurrentCache) GetSaved(key string) (any, error) {
 	val, ok := c.buff.Load(key)
-	if ok == false {
+	if !ok {
 		return val, fmt.Errorf("%w: %s", ErrMissingKey, key)
 	}
 
 	return val, nil
 }
 
+// Reset clears all map entries
 func (c *ConcurrentCache) Reset() {
 	c.buff = sync.Map{}
 }
 
+// All returns all map data
 func (c *ConcurrentCache) All() map[string]any {
 	tmpMap := make(map[string]any)
 	c.buff.Range(func(key, value any) bool {
